4ca5fe5c650015c03cdfac9591e8d4beee7dfd07
GagnonStyle <spencer.gagnon95@gmail.com>
2015-10-25 11:16:01 -0400

    Finalized location data stuff with isBetter checks

--- app/src/main/java/com/example/alec/tinlip/MainActivity.java
+++ app/src/main/java/com/example/alec/tinlip/MainActivity.java
@@ -23,8 +23,7 @@ import android.widget.TextView;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    Button submit;
-    Context context = this;
+
     public static Location currentLocation;
 
     @Override
@@ -40,7 +39,9 @@ public class MainActivity extends AppCompatActivity {
         LocationListener listener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                currentLocation = location;
+                if(isBetterLocation(location, currentLocation)){
+                    currentLocation = location;
+                }
             }
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -54,10 +55,61 @@ public class MainActivity extends AppCompatActivity {
             public void onProviderDisabled(String provider) {
 
             }
+            private static final int FIFTEEN_SECONDS = 1000 * 15;
+
+            protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+                if (currentBestLocation == null) {
+                    // A new location is always better than no location
+                    return true;
+                }
+
+                // Check whether the new location fix is newer or older
+                long timeDelta = location.getTime() - currentBestLocation.getTime();
+                boolean isSignificantlyNewer = timeDelta > FIFTEEN_SECONDS;
+                boolean isSignificantlyOlder = timeDelta < -FIFTEEN_SECONDS;
+                boolean isNewer = timeDelta > 0;
+
+                // If it's been more than two minutes since the current location, use the new location
+                // because the user has likely moved
+                if (isSignificantlyNewer) {
+                    return true;
+                    // If the new location is more than two minutes older, it must be worse
+                } else if (isSignificantlyOlder) {
+                    return false;
+                }
+
+                // Check whether the new location fix is more or less accurate
+                int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+                boolean isLessAccurate = accuracyDelta > 0;
+                boolean isMoreAccurate = accuracyDelta < 0;
+                boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+                // Check if the old and new location are from the same provider
+                boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                        currentBestLocation.getProvider());
+
+                // Determine location quality using a combination of timeliness and accuracy
+                if (isMoreAccurate) {
+                    return true;
+                } else if (isNewer && !isLessAccurate) {
+                    return true;
+                } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+                    return true;
+                }
+                return false;
+            }
+
+            private boolean isSameProvider(String provider1, String provider2) {
+                if (provider1 == null) {
+                    return provider2 == null;
+                }
+                return provider1.equals(provider2);
+            }
 
 
         };
         manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+        manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
     }
 
     @Override
--- app/src/main/java/com/example/alec/tinlip/ViewNotesActivity.java
+++ app/src/main/java/com/example/alec/tinlip/ViewNotesActivity.java
@@ -1,8 +1,10 @@
 package com.example.alec.tinlip;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -18,6 +20,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
@@ -49,8 +52,10 @@ public class ViewNotesActivity extends AppCompatActivity {
         LocationListener listener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                currentLocation = location;
-                updateText();
+                if(isBetterLocation(location, currentLocation)){
+                    currentLocation = location;
+                    updateText();
+                }
             }
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -64,10 +69,61 @@ public class ViewNotesActivity extends AppCompatActivity {
             public void onProviderDisabled(String provider) {
 
             }
+            private static final int FIFTEEN_SECONDS = 1000 * 15;
+
+            protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+                if (currentBestLocation == null) {
+                    // A new location is always better than no location
+                    return true;
+                }
+
+                // Check whether the new location fix is newer or older
+                long timeDelta = location.getTime() - currentBestLocation.getTime();
+                boolean isSignificantlyNewer = timeDelta > FIFTEEN_SECONDS;
+                boolean isSignificantlyOlder = timeDelta < -FIFTEEN_SECONDS;
+                boolean isNewer = timeDelta > 0;
+
+                // If it's been more than two minutes since the current location, use the new location
+                // because the user has likely moved
+                if (isSignificantlyNewer) {
+                    return true;
+                    // If the new location is more than two minutes older, it must be worse
+                } else if (isSignificantlyOlder) {
+                    return false;
+                }
+
+                // Check whether the new location fix is more or less accurate
+                int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+                boolean isLessAccurate = accuracyDelta > 0;
+                boolean isMoreAccurate = accuracyDelta < 0;
+                boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+                // Check if the old and new location are from the same provider
+                boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                        currentBestLocation.getProvider());
+
+                // Determine location quality using a combination of timeliness and accuracy
+                if (isMoreAccurate) {
+                    return true;
+                } else if (isNewer && !isLessAccurate) {
+                    return true;
+                } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+                    return true;
+                }
+                return false;
+            }
+
+            private boolean isSameProvider(String provider1, String provider2) {
+                if (provider1 == null) {
+                    return provider2 == null;
+                }
+                return provider1.equals(provider2);
+            }
 
 
         };
         manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+        manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
         updateText();
 
         // END LISTENER
